6359451564ec8935a3d33c38f92e54c647f3fc5d
Chad <crdesabille@gmail.com>
2020-04-15 07:18:57 +0800

    Tweak toggler text

--- popup.js
+++ popup.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const showHideBtn = document.getElementById('showHideBtn');
-let showHideBtnText = false;
+// let showHideBtnText = false;
 
 showHideBtn.onclick = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.sendMessage(tabs[0].id, { panel: 'togglePanelVisibility' });
   });
-  showHideBtnText = !showHideBtnText;
-  showHideBtn.innerText = showHideBtnText ? 'Hide Panel' : 'Show Panel';
+  // showHideBtnText = !showHideBtnText;
+  // showHideBtn.innerText = showHideBtnText ? 'Hide Panel' : 'Show Panel';
 };
 
 
 chrome.storage.sync.get(['isMainPanelVisible'], result => {
-  showHideBtn.innerText = result.isMainPanelVisible ? 'Hide Panel' : !result.isMainPanelVisible ? 'Show Panel' : 'Show Panel';
+  // showHideBtn.innerText = result.isMainPanelVisible ? 'Hide Panel' : !result.isMainPanelVisible ? 'Show Panel' : 'Show Panel';
 });
